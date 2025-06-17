c532998f3d2c172de0c1e006dad37350def08c8f
guguiness <gus.nogueira@proton.me>
2025-06-16 21:07:13 -0300

    Adicionado detector de placa

---
+++ detectors/placa_detector.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+class PlacaDetector:
+    def __init__(self, model_path="models/yolo_model/best.pt"):
+        self.model = YOLO(model_path)
+
+    def detectar(self, image):
+        results = self.model.predict(image, conf=0.5)
+        detections = []
+        for r in results:
+            for box in r.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                detections.append((x1, y1, x2, y2))
+        return detections
